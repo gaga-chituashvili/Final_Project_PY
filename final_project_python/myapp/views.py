256cd9198d1517d4b869377e33ac0733301378fb
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-03-24 23:42:35 +0400

    first commit

---
+++ final_project_python/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request):
+    return render(request,"index.html")
+
+
+
+def about_view(request):
+    return render(request,"about.html")
+
+
+def thirds_view(request):
+    return render(request,"thirds.html")
