49575a08a7131d9f9d80d6a36484b33613b859a5
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-05-01 11:36:00 +0400

    templete

--- final_project_python/settings.py
+++ final_project_python/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#&5k+-kdb!cc=00!g!^-l@n90o$q^)jsql*m@mu2ma&&^j!w#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['restaurant.pythonanywhere.com']
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
