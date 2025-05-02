9786dd744f2eefefc7e0352e803ee5284cafbf52
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-05-02 20:58:43 +0400

    change_settings

--- final_project_python/settings.py
+++ final_project_python/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-#&5k+-kdb!cc=00!g!^-l@n90o$q^)jsql*m@mu2ma&&^j!w#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static')
