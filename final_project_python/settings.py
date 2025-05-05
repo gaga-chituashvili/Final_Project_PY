dab493fcc797b0cd0abd71aaf16c2244e55bca9f
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-05-05 23:43:54 +0400

    add_style

--- final_project_python/settings.py
+++ final_project_python/settings.py
@@ -137,13 +137,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
    BASE_DIR / 'static'
 ]
 STATICFILES_DIRS = [
     BASE_DIR / "static",  
 ]
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 
 MEDIA_URL = "media/"
 
