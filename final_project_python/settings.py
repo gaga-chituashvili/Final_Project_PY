2d41808c99dcffcf4d5be1bac6d85f712aefc663
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-05-06 00:08:53 +0400

    change1331

--- final_project_python/settings.py
+++ final_project_python/settings.py
@@ -137,16 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-   BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  
-]
-
 MEDIA_URL = "media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
