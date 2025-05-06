adc8c10b952b1b09841d3853e58b34fda5c38295
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-05-07 00:10:02 +0400

    host_change

--- final_project_python/settings.py
+++ final_project_python/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-#&5k+-kdb!cc=00!g!^-l@n90o$q^)jsql*m@mu2ma&&^j!w#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['georgianrestaurant-diiwi.ondigitalocean.app']
+ALLOWED_HOSTS = ['https://georgianrestaurant-5wjf7.ondigitalocean.app']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Application definition
