174dbca0f0578cd684919a34a8cb368c3b9bfc4b
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-05-07 00:30:27 +0400

    ...

--- final_project_python/settings.py
+++ final_project_python/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#&5k+-kdb!cc=00!g!^-l@n90o$q^)jsql*m@mu2ma&&^j!w#*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['georgianrestaurant-hyd6d.ondigitalocean.app']
 
