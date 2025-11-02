8975117d712c3ecd997c7c337bab800d557d7c4a
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-11-02 14:30:22 +0400

    add cloudinary

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from users.models import User
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 class Product(models.Model):
-    thumbnil = models.ImageField(upload_to="final_project_python/", blank=True, null=True)
+    thumbnil = models.ImageField(storage=MediaCloudinaryStorage(),upload_to="final_project_python/", blank=True, null=True)
     name = models.CharField(max_length=255)
     descriptions = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
